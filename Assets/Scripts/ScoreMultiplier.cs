d20c866f0a977d9d8658c7dccb602b8612a7de4c
Gurankas SIngh <gurankas@gmail.com>
2020-10-26 23:23:05 -0700

    Determining landing (3 types) based on speed at impact with terrain

---
+++ Assets/Scripts/ScoreMultiplier.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ScoreMultiplier : MonoBehaviour
+{
+    public int ScoreMul;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<BaseCharacter>() != null)
+        {
+            other.GetComponent<BaseCharacter>().ScoreMultiplier = ScoreMul;
+        }
+    }
+}
